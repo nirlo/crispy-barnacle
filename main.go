d8a8a86856dc388a4425b690f81b99584fbbc708
nirlo <nrlockhart@gmail.com>
2019-03-01 16:50:37 -0500

    added display records and file input

--- main.go
+++ main.go
@@ -4,12 +4,29 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"reflect"
+	"strings"
 )
+
+//Species,Year,Julian Day of Year,Plant Identification Number,Number of Buds,Number of Flowers,Number of Flowers that have Reached Maturity,Observer Initials,Observer Comments
+
+// Row A struct specifically for a row of data. This is what will be pulled from the csv
+type Row struct {
+	species         string
+	year            string
+	dayOfYear       string
+	id              string
+	numberOfBuds    string
+	numberOfFlowers string
+	numberMature    string
+	initals         string
+	comments        string
+}
 
 var (
 	pathToFile = ""
 	reader     = bufio.NewReader(os.Stdin)
-	records    []string
+	records    []Row
 )
 
 func main() {
@@ -87,10 +104,43 @@ func loadFile() {
 
 	fmt.Println("Nicholas Lockhart")
 
+	f, err := os.Open(pathToFile)
+	check(err)
+
+	scanner := bufio.NewScanner(f)
+	for scanner.Scan() {
+		line := scanner.Text()
+		row := strings.Split(line, ",")
+		records = append(records, Row{row[0], row[1], row[2], row[3], row[4], row[5], row[6], row[7], row[8]})
+		if len(records) == 12 {
+			break
+		}
+	}
+
+	// Removing the column names, we won't need them were we are going
+	records = append(records[:1], records[2:]...)
+	records = append(records[:0], records[1:]...)
+
+	fmt.Println("Data from file retrieved! Nicholas Lockhart")
 }
 
 func displayRecords() {
-	//TODO display all records
+	fmt.Println("Congrats, you get to read the data that we have! Nicholas Lockhart")
+
+	ref := reflect.ValueOf(&records[0]).Elem()
+	typeOfT := ref.Type()
+
+	for i, s := range records {
+		if i == 0 {
+			for i := 0; i < ref.NumField(); i++ {
+				fmt.Println("%s\t",
+					typeOfT.Field(i).Name)
+			}
+		}
+		fmt.Println("%d: %s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t", i+1,
+			s.species, s.year, s.dayOfYear, s.id, s.numberOfBuds, s.numberOfFlowers, s.numberMature, s.initals, s.comments)
+	}
+	fmt.Println("Nicholas Lockhart")
 }
 
 func createRecord() {
